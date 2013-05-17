5854b6e6a6ac421bbae497d68433f35194cf7294
sammysf <samueltoizer2014@u.northwestern.edu>
2013-05-17 17:21:22 -0500

    Inventory buttons toggle when clicked.

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 public class PlayerInventory : MonoBehaviour {
 	private static List<Item> _inventory = new List<Item>();
 	
-	// Design decision bools, to be removed later
-	bool allButtons = true;
-	bool displayNameDesc = false;
-
+	// Decision bools, to be removed later
+	bool shouldRemove = false;
+	
+	// GUI related variables
+	public GUISkin mySkin;
+	int focusedButton = -1;
 	
 	public static List<Item> Inventory {
 		get {return _inventory;}
@@ -18,16 +20,14 @@ public class PlayerInventory : MonoBehaviour {
 	}
 	
 	public void OnGUI() {
-		if (allButtons) {
-			if (displayNameDesc)
-				GUILayout.BeginArea(new Rect(10,10,500,500));
-			else
-				GUILayout.BeginArea(new Rect(10,10,200,500));
+		GUI.skin = mySkin;
+		GUILayout.BeginArea(new Rect(10,10,200,500));
 			
-			for (int i = 0; i < _inventory.Count; i++) {
-				GUILayout.BeginHorizontal();
-				GUI.SetNextControlName("item");
-				if (GUILayout.Button(_inventory[i].icon)) {
+		for (int i = 0; i < _inventory.Count; i++) {
+			GUILayout.BeginHorizontal();
+			GUI.SetNextControlName("item" + i.ToString());
+			if (GUILayout.Button(_inventory[i].icon)) {
+				if (shouldRemove) {
 					_inventory[i].transform.position = this.transform.position;
 					_inventory[i].active = true;
 					_inventory.Remove(_inventory[i]);
@@ -35,24 +35,25 @@ public class PlayerInventory : MonoBehaviour {
 					i--;
 				}
 				else {
-					if (displayNameDesc) {
-						GUILayout.Box(_inventory[i].itemName);
-						GUILayout.EndHorizontal();
-						GUILayout.Box(_inventory[i].description);
-					}
-					else
-						GUILayout.EndHorizontal();
+					if (GUI.GetNameOfFocusedControl() != ("item" + i.ToString())) {
+							
+							GUI.FocusControl("item" + i.ToString());
+							focusedButton = i;
+							_inventory[i].isToggled = true;
+						}
+						else {
+							GUIUtility.keyboardControl = 0;
+							focusedButton = -1;
+							_inventory[i].isToggled = false;
+						}
 				}
 			}
-			GUILayout.EndArea();
+			GUILayout.EndHorizontal();
 		}
-		else {
-			Texture[] images = new Texture[_inventory.Count];
-			for (int i = 0; i < _inventory.Count; i++) {
-				images[i] = _inventory[i].icon;
-			}
-			GUI.SelectionGrid(new Rect(10,10,200,500), 0, images, 1);
-		}
+		// Refocus button if user clicks out of inventory
+		if (focusedButton >= 0)
+				GUI.FocusControl("item" + focusedButton.ToString());
+		GUILayout.EndArea();
 	}
 	
 	// This allows user to pick up item by pressing E if they are close enough to item
